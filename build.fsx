dac44c79e9ea4fa44f81b58e6b0e8c3f8f8e75ef
Harald Steinlechner <haraldsteinlechner@gmail.com>
2017-05-14 14:51:22 +0200

    added docs

--- build.fsx
+++ build.fsx
@@ -28,17 +28,63 @@ Target "Statistics" (fun () ->
         ()
 )
 
-let projInfo =
-  [ "page-description", "Fablish meets aardvark"
-    "page-author", "Aardvark Platform Team"
-    "github-link", "https://github.com/vrvis"
-    "project-name", "Aardvark.Gems?" ]
 
+
+
+let projInfo n =
+    [ "page-description", "Aardvark.Gems"
+      "page-author",  "The Aardvark Platform Team" 
+      "project-author",  "The Aardvark Platform Team" 
+      "github-link", "https://github.com/vrvis/aardvark.media"
+      "project-github", "https://github.com/vrvis/aardvark.media"
+      "project-nuget", sprintf "https://www.nuget.org/packages/%s" n
+      "root", sprintf "https://github.com/vrvis/aardvark.media/tree/master/docs/api/%s" n 
+      "project-name", n
+    ]
+
+let libDirs = ["--libDirs", "bin/Release"]
+
+
+module MyFake =
+    let CreateDocsForDlls outputDir templatesDirs projectParameters sourceRepo dllFiles = 
+        let layoutRoots = String.concat " " (templatesDirs |> Seq.map (sprintf "\"%s\""))
+        for file in dllFiles do
+            projectParameters
+            |> Seq.map (fun (k, v) -> [ k; v ])
+            |> Seq.concat
+            |> Seq.append 
+                    ([ "metadataformat"; "--generate"; "--outdir"; outputDir; "--layoutroots"; layoutRoots;
+                        "--sourceRepo"; sourceRepo; "--sourceFolder"; currentDirectory; "--parameters" ])
+            |> Seq.map (fun s -> 
+                    if s.StartsWith "\"" then s
+                    else sprintf "\"%s\"" s)
+            |> separated " "
+            |> fun prefix -> sprintf "%s --dllfiles \"%s\"" prefix file
+            |> Fake.FSharpFormatting.run
+
+            printfn "Successfully generated docs for DLL %s" file
+
+Target "API" (fun () -> 
+    let projects = ["Aardvark.UI"; "Aardvark.UI.Primitives";]
+    for p in projects do
+        let target = sprintf "docs/api/%s" p
+        if Directory.Exists target then ()
+        else Directory.CreateDirectory target |> ignore
+        MyFake.CreateDocsForDlls target ["docs/templates/"; "docs/templates/reference/"] (projInfo p @ libDirs) "https://github.com/vrvis/aardvark.base" [sprintf "bin/Release/%s.dll" p]
+        let logo = Path.Combine(target, "logo.png")
+        if File.Exists logo |> not then File.Copy("docs/logo.png", logo) |> ignore
+
+)
 
 Target "Docs" (fun () -> 
-    Fake.FSharpFormatting.CreateDocs "src/Aardvark.Service.Demo/docs" "docs" "src/Aardvark.Service.Demo/docs/template.html" projInfo
+    Fake.FSharpFormatting.CreateDocs "docs" "docs" "docs/template.html" (projInfo "Aardvark.Base")
     Fake.FileHelper.CopyRecursive "packages/build/FSharp.Formatting.CommandTool/styles" "docs/content" true |> printfn "copied: %A"
 )
+
+
+"Compile" ==> "Docs"
+"Compile" ==> "Docs" ==> "API"
+
 
 
 entry()
