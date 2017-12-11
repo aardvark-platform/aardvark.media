6b9400dc8b0b16a859b3ec45213ca3db7ecf6789
walch <walch@vrvis.at>
2017-12-11 12:29:10 +0100

    added missing javascript testfile

---
+++ src/Aardvark.UI.Primitives/d3Test.js
@@ -0,0 +1,55 @@
+function D3TestFunktion(input, id)
+{
+	
+var data = input;
+var series = d3.stack()
+    .keys(["apples", "bananas", "cherries", "dates"])
+    .offset(d3.stackOffsetDiverging)
+    (data);
+
+var svg = d3.select(id),
+    margin = {top: 20, right: 30, bottom: 30, left: 60},
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+var x = d3.scaleBand()
+    .domain(data.map(function(d) { return d.month; }))
+    .rangeRound([margin.left, width - margin.right])
+    .padding(0.1);
+
+var y = d3.scaleLinear()
+    .domain([d3.min(series, stackMin), d3.max(series, stackMax)])
+    .rangeRound([height - margin.bottom, margin.top]);
+
+var z = d3.scaleOrdinal(d3.schemeCategory10);
+
+svg.append("g")
+  .selectAll("g")
+  .data(series)
+  .enter().append("g")
+    .attr("fill", function(d) { return z(d.key); })
+  .selectAll("rect")
+  .data(function(d) { return d; })
+  .enter().append("rect")
+    .attr("width", x.bandwidth)
+    .attr("x", function(d) { return x(d.data.month); })
+    .attr("y", function(d) { return y(d[1]); })
+    .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+
+svg.append("g")
+    .attr("transform", "translate(0," + y(0) + ")")
+    .call(d3.axisBottom(x));
+
+svg.append("g")
+    .attr("transform", "translate(" + margin.left + ",0)")
+    .call(d3.axisLeft(y));
+
+function stackMin(serie) {
+  return d3.min(serie, function(d) { return d[0]; });
+}
+
+function stackMax(serie) {
+  return d3.max(serie, function(d) { return d[1]; });
+}
+				
+}
