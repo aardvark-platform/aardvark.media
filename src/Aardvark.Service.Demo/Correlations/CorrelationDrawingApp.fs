be3c450320f46f22ef84c6d6271b1aa94bccff4d
Rebecca Nowak <rnowak@vrvis.at>
2018-02-20 10:18:29 +0100

    missed some files in previous commit

--- src/Aardvark.Service.Demo/Correlations/CorrelationDrawingApp.fs
+++ src/Aardvark.Service.Demo/Correlations/CorrelationDrawingApp.fs
@@ -1,7 +1,6 @@
-﻿namespace UI.Composed
+﻿namespace CorrelationDrawing
 
 open System
-
 open Aardvark.Base
 open Aardvark.Base.Geometry
 open Aardvark.Base.Incremental
@@ -11,19 +10,39 @@ open Aardvark.Application
 open Aardvark.SceneGraph
 open Aardvark.UI
 open Aardvark.UI.Primitives
+open UI.Composed
 open Aardvark.Rendering.Text
-
-open PRo3DModels
 
 open Aardvark.SceneGraph.SgPrimitives
 open Aardvark.SceneGraph.FShadeSceneGraph
        
+
+module Serialization =
+    open MBrace.FsPickler
+    open System.IO
+    let binarySerializer = FsPickler.CreateBinarySerializer()
+    
+    let save (model : CorrelationAppModel) path = 
+        let arr = binarySerializer.Pickle model
+        File.WriteAllBytes(path, arr);
+
+    let load path : CorrelationAppModel = 
+        let arr = File.ReadAllBytes(path);
+        let app = binarySerializer.UnPickle arr
+        app
+
+    let writeToFile path (contents : string) =
+        System.IO.File.WriteAllText(path, contents)
+
 module CorrelationDrawingApp = 
     open Newtonsoft.Json
             
     type Action =
         | CameraMessage    of ArcBallController.Message
-        | DrawingMessage   of Drawing.Action
+        | DrawingMessage   of CorrelationDrawing.Action
+        | SemanticMessage of Semantic.Action
+        | AddSemantic of CorrelationDrawing.Action
+//        | EditSemantic of CorrelationDrawing.Action
         | KeyDown of key : Keys
         | KeyUp of key : Keys      
         | Export
@@ -33,18 +52,22 @@ module CorrelationDrawingApp =
         | Undo
         | Redo
                        
-    let stash (model : AnnotationAppModel) =
+    let stash (model : CorrelationAppModel) =
         { model with history = Some model; future = None }
 
-    let clearUndoRedo (model : AnnotationAppModel) =
+    let clearUndoRedo (model : CorrelationAppModel) =
         { model with history = None; future = None }
 
-    let update (model : AnnotationAppModel) (act : Action) =
+    let update (model : CorrelationAppModel) (act : Action) =
         match act, model.drawing.draw with
             | CameraMessage m, false -> 
                     { model with camera = ArcBallController.update model.camera m }      
             | DrawingMessage m, _ ->
-                    { model with drawing = Drawing.update model.drawing m }           
+                    { model with drawing = CorrelationDrawing.update model.drawing m }    
+            | AddSemantic m, false ->
+                {model with drawing = CorrelationDrawing.update model.drawing m}       
+//            | EditSemantic cs, _ ->
+//                    { model with drawing = CorrelationDrawing.update model.drawing cs}
             | Save, _ -> 
                     Serialization.save model ".\drawing"
                     model
@@ -60,17 +83,18 @@ module CorrelationDrawingApp =
                 match model.future with
                     | Some f -> f
                     | None -> model
+            // | EditSemantics, _ ->  
             | KeyDown k, _ -> 
-                    let d = Drawing.update model.drawing (Drawing.Action.KeyDown k)
+                    let d = CorrelationDrawing.update model.drawing (CorrelationDrawing.Action.KeyDown k)
                     { model with drawing = d }
             | KeyUp k, _ -> 
-                    let d = Drawing.update model.drawing (Drawing.Action.KeyUp k)
+                    let d = CorrelationDrawing.update model.drawing (CorrelationDrawing.Action.KeyUp k)
                     { model with drawing = d }
             | _ -> model
                        
     let myCss = { kind = Stylesheet; name = "semui-overrides"; url = "semui-overrides.css" }
     
-    let view (model : MAnnotationAppModel) =
+    let view (model : MCorrelationAppModel) =
                     
         let frustum =
             Mod.constant (Frustum.perspective 60.0 0.1 100.0 1.0)
@@ -88,10 +112,11 @@ module CorrelationDrawingApp =
                                     attribute "style" "width:65%; height: 100%; float: left;"]
                         )
                         (
-                            Drawing.Sg.view model.drawing model.camera.view
+                            CorrelationDrawing.Sg.view model.drawing model.camera.view
                                 |> Sg.map DrawingMessage
-                                |> Sg.fillMode model.rendering.fillMode
-                                |> Sg.cullMode model.rendering.cullMode                                                                         
+                                |> Sg.fillMode (model.rendering.fillMode)
+                                    
+                                |> Sg.cullMode (model.rendering.cullMode)                                                                    
                         )                                        
                 ]
             
@@ -99,45 +124,48 @@ module CorrelationDrawingApp =
                     
                     div [clazz "ui buttons inverted"] [
                         button [clazz "ui icon button"; onMouseClick (fun _ -> Save)] [
-                                    i [clazz "save icon"] [] ]
+                                    i [clazz "save icon"] [] ] |> Utilities.wrapToolTip "save"
                         button [clazz "ui icon button"; onMouseClick (fun _ -> Load)] [
-                                    i [clazz "folder outline icon"] [] ]
+                                    i [clazz "folder outline icon"] [] ] |> Utilities.wrapToolTip "load"
                         button [clazz "ui icon button"; onMouseClick (fun _ -> Clear)] [
-                                    i [clazz "file outline icon"] [] ]
+                                    i [clazz "file outline icon"] [] ] |> Utilities.wrapToolTip "clear"
                         button [clazz "ui icon button"; onMouseClick (fun _ -> Export)] [
-                                    i [clazz "external icon"] [] ]
+                                    i [clazz "external icon"] [] ] |> Utilities.wrapToolTip "export"
                         button [clazz "ui icon button"; onMouseClick (fun _ -> Undo)] [
-                                    i [clazz "arrow left icon"] [] ]
+                                    i [clazz "arrow left icon"] [] ] |> Utilities.wrapToolTip "undo"
                         button [clazz "ui icon button"; onMouseClick (fun _ -> Redo)] [
-                                    i [clazz "arrow right icon"] [] ]
+                                    i [clazz "arrow right icon"] [] ] |> Utilities.wrapToolTip "redo"
+                        button [clazz "ui icon button"; onMouseClick (fun _ -> Action.AddSemantic CorrelationDrawing.AddSemantic)] [
+                                    i [clazz "plus icon"] [] ] //|> Utilities.wrapToolTip "add semantic"
                     ]
 
-                    Drawing.UI.viewAnnotationTools model.drawing |> UI.map DrawingMessage
-                    Drawing.UI.viewAnnotations model.drawing               
+                    CorrelationDrawing.UI.viewAnnotationTools model.drawing |> UI.map DrawingMessage
+                    CorrelationDrawing.UI.viewAnnotations model.drawing    
+                    CorrelationDrawing.UI.viewSemantics model.drawing
                 ]
             ]
         )
    
-    let initialdrawing = {
-        hoverPosition = None
-        draw = false            
+    //let initialdrawing = {
+        //hoverPosition = None
+        //draw = false            
 
-        working = None
-        projection = Projection.Viewpoint
-        geometry = Geometry.Polyline
-        semantic = Semantic.Horizon3
+        //working = None
+        //projection = Projection.Viewpoint
+        //geometry = GeometryType.Polyline
+        //semantic = Semantic.initial
 
-        annotations = PList.empty
+        //annotations = PList.empty
 
-        exportPath = @"."
-    }
+        //exportPath = @"."
+    //}
 
-    let initial : AnnotationAppModel =
+    let initial : CorrelationAppModel =
         {
             camera           = { ArcBallController.initial with view = CameraView.lookAt (23.0 * V3d.OIO) V3d.Zero V3d.OOI}
-            rendering        = InitValues.rendering
+            rendering        = RenderingPars.initial
            
-            drawing = initialdrawing
+            drawing = CorrelationDrawing.insertFirstSemantics CorrelationDrawing.initial
 
            
 
@@ -145,7 +173,7 @@ module CorrelationDrawingApp =
             future = None
         }
 
-    let app : App<AnnotationAppModel,MAnnotationAppModel,Action> =
+    let app : App<CorrelationAppModel,MCorrelationAppModel,Action> =
         {
             unpersist = Unpersist.instance
             threads = fun model -> ArcBallController.threads model.camera |> ThreadPool.map CameraMessage
