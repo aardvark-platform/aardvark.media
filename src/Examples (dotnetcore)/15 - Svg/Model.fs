b5a0f94246cccf323e6f9fa582a8d920232eb8c9
Harald Steinlechner <haraldsteinlechner@gmail.com>
2018-05-08 10:31:23 +0200

    added svg drag example

---
+++ src/Examples (dotnetcore)/15 - Svg/Model.fs
@@ -0,0 +1,16 @@
+﻿namespace Model
+
+open Aardvark.Base
+open Aardvark.Base.Incremental
+open Aardvark.UI.Primitives
+
+type Message = 
+    | StartDrag of V2d
+    | Drag of V2d
+
+[<DomainType>]
+type Model = 
+    {
+        startPos : Option<V2d>
+        pos : V2d
+    }
