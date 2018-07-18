cd31a7c91f341e3be561a9b8c0115c9426be3d13
Harald Steinlechner <haraldsteinlechner@gmail.com>
2018-07-18 18:01:29 +0200

    sync

--- src/Examples (dotnetcore)/02 - RenderControl/Program.fs
+++ src/Examples (dotnetcore)/02 - RenderControl/Program.fs
@@ -18,7 +18,7 @@ let main argv =
 
     // media apps require a runtime, which serves as renderer for your render controls.
     // you can use OpenGL or VulkanApplication.
-    let useVulkan = false
+    let useVulkan = true
 
     let runtime, disposable =
         if useVulkan then
@@ -34,16 +34,16 @@ let main argv =
     let instance = 
         app |> App.start
 
-    WebPart.startServer 4321 [ 
+    WebPart.runServer 80 [ 
         MutableApp.toWebPart' runtime false instance
         Suave.Files.browseHome
     ]  
     
 
-    Aardium.run {
-        url "http://localhost:4321/"
-        width 1024
-        height 768
-        debug true
-    }
+    //Aardium.run {
+    //    url "http://localhost:4321/"
+    //    width 1024
+    //    height 768
+    //    debug true
+    //}
     0 
