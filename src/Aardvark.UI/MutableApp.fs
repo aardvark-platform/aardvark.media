76b5e3c60573ab01b5647aafdafac0dbf1f400dd
Harald Steinlechner <haraldsteinlechner@gmail.com>
2018-09-05 17:31:28 +0200

    updated packages

--- src/Aardvark.UI/MutableApp.fs
+++ src/Aardvark.UI/MutableApp.fs
@@ -41,20 +41,23 @@ module private Tools =
 
 // https://github.com/aardvark-platform/aardvark.media/issues/19
 module ThreadPoolAdjustment =
+
+    let mutable shouldAdjust = true
     
     let adjust () =
-        let mutable maxThreads,maxIOThreads = 0,0
-        System.Threading.ThreadPool.GetMaxThreads(&maxThreads,&maxIOThreads)
-        let mutable minThreads, minIOThreads = 0,0
-        System.Threading.ThreadPool.GetMinThreads(&minThreads,&minIOThreads)
-        if minThreads < 12 || minIOThreads < 12 then
-            Log.warn "[aardvark.media] currently ThreadPool.MinThreads is (%d,%d)" minThreads minIOThreads
-            let minThreads   = max 12 minThreads
-            let minIOThreads = max 12 minIOThreads
-            Log.warn "[aardvark.media] unfortunately, currently we need to adjust this to at least (12,12) due to an open issue https://github.com/aardvark-platform/aardvark.media/issues/19"
-            if not <| System.Threading.ThreadPool.SetMinThreads(minThreads, minIOThreads) then Log.warn "could not set min threads"
-            if maxThreads < 12 || maxIOThreads < 12 then
-                Log.warn "[aardvark.media] detected less than 12 threadpool threads: (%d,%d). Be aware that this will result in severe stutters... Consider switching back to the default (65537,1000)." maxThreads maxIOThreads
+        if shouldAdjust then
+            let mutable maxThreads,maxIOThreads = 0,0
+            System.Threading.ThreadPool.GetMaxThreads(&maxThreads,&maxIOThreads)
+            let mutable minThreads, minIOThreads = 0,0
+            System.Threading.ThreadPool.GetMinThreads(&minThreads,&minIOThreads)
+            if minThreads < 12 || minIOThreads < 12 then
+                Log.warn "[aardvark.media] currently ThreadPool.MinThreads is (%d,%d)" minThreads minIOThreads
+                let minThreads   = max 12 minThreads
+                let minIOThreads = max 12 minIOThreads
+                Log.warn "[aardvark.media] unfortunately, currently we need to adjust this to at least (12,12) due to an open issue https://github.com/aardvark-platform/aardvark.media/issues/19"
+                if not <| System.Threading.ThreadPool.SetMinThreads(minThreads, minIOThreads) then Log.warn "could not set min threads"
+                if maxThreads < 12 || maxIOThreads < 12 then
+                    Log.warn "[aardvark.media] detected less than 12 threadpool threads: (%d,%d). Be aware that this will result in severe stutters... Consider switching back to the default (65537,1000)." maxThreads maxIOThreads
 
 
 module MutableApp =
