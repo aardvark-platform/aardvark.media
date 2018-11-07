f820051066288b260d50c85c78debe59d0ea23fc
ortner <ortner@vrvis.at>
2018-11-07 18:51:20 +0100

    switched to typeproviders

--- src/Scratch/11 - GeoJsonViewer/GeoJSON.fs
+++ src/Scratch/11 - GeoJsonViewer/GeoJSON.fs
@@ -5,18 +5,15 @@ open FSharp.Data
 open FSharp.Data.JsonExtensions
 
 module GeoJSON =
+  
+  type EoxModel = JsonProvider<"..\..\..\data\eox.json">
 
   let load (jsonFile : string) : Model = 
-    let root = JsonValue.Load(jsonFile)
-    
-    match root with 
-    | JsonValue.Record r -> ()
-      
-      //let typus, boundingBox, features = root?type, info?bbox, info?features
-      //Log.line "%A" r
-    | _ -> Log.line "%A" root
-    
-    { value = 0 }
+    let eox = EoxModel.Load(jsonFile)
+            
+    Log.line "%A %A %A" eox.Bbox eox.Type eox.Features
+
+    failwith ""
 
 
 
