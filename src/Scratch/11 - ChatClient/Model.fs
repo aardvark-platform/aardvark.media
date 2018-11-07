b056e340ca934cc999525df3db59d02836e4430c
aszabo <aszabo@vrvis.at>
2018-11-07 17:56:05 +0100

    [Example] ChatClient

---
+++ src/Scratch/11 - ChatClient/Model.fs
@@ -0,0 +1,31 @@
+﻿namespace Chat
+
+open Aardvark.Base
+open Aardvark.Base.Incremental
+open Aardvark.UI.Primitives
+
+open System
+
+
+type Client = 
+    {
+        id : string
+        name : string
+    }
+
+[<DomainType>]
+type Model = 
+    {
+        lines : hmap<DateTime, string>
+        clients : hmap<string, Client>
+        currentMsg : hmap<string, string>
+    }
+
+module Model =
+    let initial =
+        {
+            lines = HMap.empty
+            clients = HMap.empty
+            currentMsg = HMap.empty
+        }
+
