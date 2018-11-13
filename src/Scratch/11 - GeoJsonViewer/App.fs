b05bc7b0974eca5e0c6666216355511346ff31a2
ortner <ortner@vrvis.at>
2018-11-13 16:42:03 +0100

    fighting with svg coordinate systems

--- src/Scratch/11 - GeoJsonViewer/App.fs
+++ src/Scratch/11 - GeoJsonViewer/App.fs
@@ -40,6 +40,74 @@ module App =
           yield item 
       }
 
+    let inline (==>) a b = Aardvark.UI.Attributes.attribute a b
+
+    //<svg width="400" height="110">
+    //  <rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+    //</svg> 
+
+    let svgDrawBoundingBox attributes (canvasSize : V2d)(box : Box2d) =
+      
+      Svg.rect <| attributes @ [
+            "x" ==> sprintf "%f"      box.Min.X
+            "y" ==> sprintf "%f"      box.Min.Y
+            "width" ==> sprintf  "%f" box.SizeX
+            "height" ==> sprintf "%f" box.SizeY
+            
+        ]
+    
+    let svgGlobalBBStyle = 
+      [ 
+        "fill" ==> "none"
+        "stroke" ==> "#A8814C"
+        "stroke-width" ==> "2"
+        "stroke-opacity" ==> "0.1"
+      ]
+
+    let svgBBStyle = 
+      [ 
+        "fill" ==> "blue"
+        "stroke" ==> "darkblue"
+        "stroke-width" ==> "1"        
+        "fill-opacity" ==> "0.1"
+      ]
+
+    let svgDrawBoundingBoxNorm attributes (globalBox : Box2d) (canvasSize : V2d) (box : Box2d) =
+      let trafo = 
+        (Trafo2d.Translation -globalBox.Min) *
+        (Trafo2d.Scale (canvasSize / globalBox.Size))
+      let b = box.Transformed(trafo) 
+      b|> svgDrawBoundingBox attributes canvasSize
+
+    let svgDrawFeature (globalBox : Box2d) (scale : V2d) (feature : Feature) =     
+      feature.boundingBox |> svgDrawBoundingBoxNorm svgBBStyle globalBox scale
+
+    let svg = 
+      
+      let canvasSize = V2d(640.0, 480.0)
+
+      let blarg (bb : IMod<Box2d>)= 
+        amap {
+          //let! bb = bb
+          yield "width" ==> sprintf "%f" canvasSize.X
+          yield "height" ==> sprintf "%f" canvasSize.Y
+          yield "viewBox" ==> sprintf ("%f %f %f %f") 0.0 (-canvasSize.Y) canvasSize.X canvasSize.Y
+          yield clazz "svgRoot"
+          yield style "border: 2px dashed black"
+
+        } |>  AttributeMap.ofAMap
+      
+      let content =
+        alist {
+          let! bb = model.data.boundingBox
+          yield bb |> svgDrawBoundingBoxNorm svgGlobalBBStyle bb canvasSize
+
+          for f in model.data.features do
+            yield f |> svgDrawFeature bb canvasSize
+        }
+
+      Incremental.Svg.svg (blarg model.data.boundingBox) content
+
     page (fun request ->
       match Map.tryFind "page" request.queryParams with
         | Some "list" ->
@@ -48,6 +116,8 @@ module App =
                 Incremental.div ([clazz "ui very compact stackable inverted relaxed divided list"] |> AttributeMap.ofList) content
               ]
             )
+        | Some "map" ->            
+            body [ style "width: 100%; height:100%; background: #636363; overflow: hidden"] [svg]            
         | Some other -> 
           let msg = sprintf "Unknown page: %A" other
           body [] [
@@ -83,7 +153,7 @@ module App =
                   content (
                       horizontal 10.0 [
                           element { id "map";  title "2D Overview"; weight 3; isCloseable false }
-                          element { id "list"; title "Features";    weight 2; isCloseable false }
+                          element { id "list"; title "Features";    weight 1; isCloseable false }
                       ]
                   )
                   appName "GeoJSON"
