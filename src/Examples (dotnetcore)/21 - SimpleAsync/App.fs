d8badea0a8ddccb47e482aec607390342aa65af2
Harald Steinlechner <haraldsteinlechner@gmail.com>
2019-01-16 14:24:52 +0100

    added another async example

---
+++ src/Examples (dotnetcore)/21 - SimpleAsync/App.fs
@@ -0,0 +1,57 @@
+﻿module RenderControl.App
+
+open Aardvark.UI
+open Aardvark.UI.Primitives
+
+open Aardvark.Base
+open Aardvark.Base.Incremental
+open Aardvark.Base.Rendering
+open RenderControl.Model
+
+
+let update (model : Model) (msg : Message) =
+    match msg with
+        | Log s -> 
+            { model with info = s }
+        | Done(id,r) -> 
+            { model with result = r; threads = ThreadPool.remove id model.threads }
+        | Start -> 
+            let id = System.Guid.NewGuid().ToString()
+            let worker =    
+                proclist {
+                    yield Log "Begin"
+                    do! Proc.Sleep 100
+                    for i in 0 .. 100 do
+                        do! Proc.Sleep 10
+                        yield Log (sprintf "got: %d%%" i)
+                    yield Done(id,System.Random().NextDouble())
+                }
+            { model with threads = ThreadPool.start worker model.threads }
+        
+
+let view (model : MModel) =
+    div [] [
+        button [onClick (fun _ -> Start)] [text "start"]
+        Incremental.text (Mod.map string model.result)
+        br []
+        Incremental.text model.info
+    ]
+
+
+let threads (model : Model) = 
+    model.threads
+
+
+let app =                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+    {
+        unpersist = Unpersist.instance     
+        threads = threads 
+        initial = 
+            { 
+                threads = ThreadPool.empty
+                info = ""
+                result = 1.0
+            }
+        update = update 
+        view = view
+    }
