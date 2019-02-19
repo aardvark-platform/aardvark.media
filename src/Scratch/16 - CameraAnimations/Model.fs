221ad04b5c0e201a60cae36cc33994ab71180fdb
ortner <ortner@vrvis.at>
2019-02-19 10:45:13 +0100

    added cameraanimations scratch

---
+++ src/Scratch/16 - CameraAnimations/Model.fs
@@ -0,0 +1,19 @@
+﻿namespace CameraAnimations
+
+open Aardvark.Base
+open Aardvark.Base.Incremental
+open Aardvark.UI.Primitives
+
+type Message = 
+  | Camera           of FreeFlyController.Message
+  | KeyUp            of key : Aardvark.Application.Keys
+  | KeyDown          of key : Aardvark.Application.Keys
+  | UpdateDockConfig of DockConfig
+
+[<DomainType>]
+type Model = 
+  {
+      cameraState          : CameraControllerState          
+      threads              : ThreadPool<Message>
+      dockConfig           : DockConfig
+  }
