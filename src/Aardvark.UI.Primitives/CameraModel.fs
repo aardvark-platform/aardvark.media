4cb6201c902fe7db899cef60c28a92eb70424293
krauthaufen <krauthaufen@awx.at>
2019-02-24 09:43:11 +0100

    [Primitives] simple orbit controller

--- src/Aardvark.UI.Primitives/CameraModel.fs
+++ src/Aardvark.UI.Primitives/CameraModel.fs
@@ -107,3 +107,33 @@ type CameraControllerState =
         stash : Option<CameraControllerState> 
     }
     
+
+
+[<DomainType>]
+type OrbitState =
+    {
+        sky     : V3d
+        center  : V3d
+        phi     : float
+        theta   : float
+        radius  : float
+
+        targetPhi : float
+        targetTheta : float
+        targetRadius : float
+        targetCenter : V3d
+        
+        dragStart : Option<V2i>
+        [<NonIncremental>]
+        lastRender : Option<MicroTime>
+
+        view : CameraView
+        
+        radiusRange : Range1d
+        thetaRange : Range1d
+        moveSensitivity : float
+        zoomSensitivity : float
+        speed : float
+
+    }
+
