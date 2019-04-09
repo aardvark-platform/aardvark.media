04eace1a8328dc7f8b7aa0e2590b82e389fe5244
Laura Fritz <laura@vrvis.at>
2019-04-09 11:00:37 +0200

    footprints

---
+++ src/Scratch/22 - FootPrint/App.fs
@@ -0,0 +1,128 @@
+﻿module App
+
+open Aardvark.UI
+open Aardvark.UI.Primitives
+
+open Aardvark.Base
+open Aardvark.Base.Incremental
+open Aardvark.Base.Rendering
+open Model
+open Utils
+open Aardvark.UI
+open Aardvark.UI.Primitives
+open Aardvark.SceneGraph
+
+
+let update2ndCam (model : Model) =
+    let right = - model.up.Cross(model.lookAt)
+    let rotPanFw = Rot3d(model.up, model.pan.value.RadiansFromDegrees()).TransformDir(model.lookAt )
+
+    let lookAt = Rot3d(right, model.tilt.value.RadiansFromDegrees()).TransformDir(rotPanFw )
+    let up = Rot3d(right, model.tilt.value.RadiansFromDegrees()).TransformDir(model.up)
+
+    let view = CameraView.look model.position.value lookAt.Normalized up.Normalized 
+    { model with camera2 = { model.camera2 with view = view } }
+
+let update (model : Model) (msg : Message) =
+    match msg with
+        | Camera m -> 
+            { model with cameraMain = FreeFlyController.update model.cameraMain m }
+        | CenterScene -> 
+            { model with cameraMain = Init.initialCamera }
+        | ChangePan p -> 
+            let m' = { model with pan = Numeric.update model.pan p }
+            update2ndCam m'
+        | ChangeTilt t -> 
+            let m' = { model with tilt = Numeric.update model.tilt t }
+            update2ndCam m'
+        | ChangeZoom z -> 
+            let m' = { model with zoom = Numeric.update model.zoom z }
+            m'
+        | ChangePosition p ->
+            let p' = Vector3d.update model.position p
+            let m' = { model with position =  p' }
+            update2ndCam m'
+        | UpdateDockConfig cfg ->
+            { model with dockConfig = cfg }
+
+
+
+let view (model : MModel) =
+
+    let viewV3dInput (model : MV3dInput) =  
+            Html.table [                            
+                Html.row "X" [Numeric.view' [InputBox] model.x |> UI.map Vector3d.Action.SetX]
+                Html.row "Y" [Numeric.view' [InputBox] model.y |> UI.map Vector3d.Action.SetY]
+                Html.row "Z" [Numeric.view' [InputBox] model.z |> UI.map Vector3d.Action.SetZ]
+            ]       
+
+    let renderControl =
+      FreeFlyController.controlledControl model.cameraMain Camera (Frustum.perspective 60.0 0.1 100.0 1.0 |> Mod.constant) 
+        (AttributeMap.ofList [ 
+            attribute "showFPS" "true"; 
+            attribute "data-renderalways" "1"; 
+            attribute "data-samples" "8"; 
+            style "width: 100%; height:80%"])
+        (Scene.fullScene model)
+    
+    // WACL
+    //<Resolution>1024, 1024</Resolution>
+    let height = "height:" + (1024/2).ToString() + ";" ///uint32(2)
+    let width = "width:" + (1024/2).ToString() + ";" ///uint32(2)
+
+    let renderControl2ndCam =
+      FreeFlyController.controlledControl model.camera2 Camera (model.frustumCam2) 
+        (AttributeMap.ofList [ 
+            attribute "showFPS" "true"; 
+            attribute "data-renderalways" "1"; 
+            attribute "data-samples" "8"; 
+            style ("background: #1B1C1E;" + height + width)])
+        (Scene.camScene model)
+
+    page (fun request -> 
+        match Map.tryFind "page" request.queryParams with
+        | Some "render" ->
+          require Html.semui ( 
+              div [clazz "ui"; style "background: #1B1C1E"] [renderControl]
+          )
+        | Some "2ndCam" ->
+          require Html.semui ( 
+              div [clazz "ui"; style "background: #1B1C1E"] [renderControl2ndCam]
+          )
+        | Some "controls" -> 
+          require Html.semui (
+            body [style "width: 100%; height:100%; background: transparent";] [
+               div[style "color:white"][
+                    button [onClick (fun _ -> CenterScene)] [text "Center Scene.."]
+                    div[style "width:400px"][
+                        Html.table [
+                          Html.row "Pan:" [Numeric.view' [NumericInputType.Slider] model.pan |> UI.map ChangePan ]  
+                          Html.row "Tilt:" [Numeric.view' [NumericInputType.Slider] model.tilt |> UI.map ChangeTilt ] 
+                          //Html.row "Zoom:" [Numeric.view' [NumericInputType.Slider] model.zoom |> UI.map ChangeZoom ] 
+                          Html.row "Position:" [viewV3dInput model.position |> UI.map ChangePosition ]
+                        ]
+                      ]
+               ]
+            ]
+          )
+        | Some other -> 
+          let msg = sprintf "Unknown page: %A" other
+          body [] [
+              div [style "color: white; font-size: large; background-color: red; width: 100%; height: 100%"] [text msg]
+          ]  
+        | None -> 
+          model.dockConfig
+            |> docking [
+              style "width:100%; height:100%; background:#F00"
+              onLayoutChanged UpdateDockConfig ]
+        )
+        
+
+let app =                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+    {
+        unpersist = Unpersist.instance     
+        threads = fun _ -> ThreadPool.empty  
+        initial = Init.initialModel
+        update = update 
+        view = view
+    }
---
+++ src/Scratch/22 - FootPrint/Model.fs
@@ -0,0 +1,118 @@
+﻿namespace Model
+
+open Aardvark.Base
+open Aardvark.Base.Incremental
+open Aardvark.UI
+open Aardvark.UI.Primitives
+open System
+open Aardvark.Base
+open Aardvark.Base
+
+type Message = 
+    | Camera            of FreeFlyController.Message
+    | CenterScene
+    | ChangePan         of Numeric.Action
+    | ChangeTilt        of Numeric.Action
+    | ChangeZoom        of Numeric.Action
+    | ChangePosition    of Vector3d.Action
+    | UpdateDockConfig  of DockConfig 
+
+[<DomainType>]
+type Model = 
+    {
+        cameraMain     : CameraControllerState
+        camera2        : CameraControllerState
+        pan            : NumericInput
+        tilt           : NumericInput
+        zoom           : NumericInput
+        position       : V3dInput
+        lookAt         : V3d
+        up             : V3d
+        frustumCam2    : Frustum
+        dockConfig     : DockConfig
+    }
+
+module Init =
+    let pan = 
+      {
+        min    = 0.0
+        max    = 360.0
+        step   = 1.0
+        format = "{0:0.0}"
+        value  = 180.0
+      }
+
+    let tilt = 
+      {
+        min    = 0.0
+        max    = 360.0
+        step   = 1.0
+        format = "{0:0.0}"
+        value  = 180.0
+      }
+
+    let zoom = 
+      {
+        min    = 0.0
+        max    = 20.0
+        step   = 1.0
+        format = "{0:0.0}"
+        value  = 0.0
+      }
+
+    let positionInput = {
+            value   = 0.0
+            min     = Double.MinValue
+            max     = Double.MaxValue
+            step    = 0.01
+            format  = "{0:0.00}"
+        }
+
+    let position (v : V3d) = {
+            x = { positionInput with value = v.X }
+            y = { positionInput with value = v.Y }
+            z = { positionInput with value = v.Z }
+            value = v    
+        }
+
+    let initialCamera = { 
+        FreeFlyController.initial' 3.0 with 
+            view = CameraView.lookAt (V3d.III * 3.0) V3d.OOO V3d.OOI
+    }
+
+    let initialCamera2 = { 
+        FreeFlyController.initial' 3.0 with 
+            view = CameraView.lookAt (V3d(0.0, 2.0, 1.0)) V3d.OOO V3d.OOI
+    }
+
+    // WACL
+    //<FocalLengthPx>1769.2, 1769.2</FocalLengthPx>
+    //<!-- angle (gon) for camera horizontal FoV-->
+    //<HorizontalFoV>35.87</HorizontalFoV>
+    //<Resolution>1024, 1024</Resolution>
+    let ifrustum = Frustum.perspective ((35.87).DegreesFromGons()) 0.001 100.0 1.0
+
+    let initialModel : Model = 
+        { 
+            cameraMain     = initialCamera
+            camera2        = initialCamera2
+            pan            = pan
+            tilt           = tilt
+            zoom           = zoom
+            position       = position (V3d(0.0, 2.0, 1.0))                       
+            lookAt         = initialCamera2.view.Forward
+            up             = initialCamera2.view.Up
+            frustumCam2    = ifrustum
+            dockConfig     =
+                config {
+                    content (
+                        horizontal 10.0 [
+                            element { id "render"; title "MainView"; weight 7.0 }
+                            element { id "2ndCam"; title "2ndView"; weight 5.0 }
+                            element { id "controls"; title "Controls"; weight 4.0 }                                                    
+                        ]
+                    )
+                    appName "FootPrintViewer"
+                    useCachedConfig true
+                }
+        }
