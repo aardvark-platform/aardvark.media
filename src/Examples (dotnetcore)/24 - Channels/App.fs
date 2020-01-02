d5eb1ec48bd4412ce6ed0eabd644e85b27a289ec
Harald Steinlechner <haraldsteinlechner@gmail.com>
2020-01-02 15:26:17 +0100

    cleaned RenderControl example

--- src/Examples (dotnetcore)/24 - Channels/App.fs
+++ src/Examples (dotnetcore)/24 - Channels/App.fs
@@ -15,11 +15,15 @@ let initialCamera = {
     }
 
 let update (model : Model) (msg : Message) =
+    printfn "%A" msg
     match msg with
         | Camera m -> 
             { model with cameraState = FreeFlyController.update model.cameraState m }
         | CenterScene -> 
             { model with cameraState = initialCamera }
+        | SetFiles s -> 
+            printfn "open file: %A" s
+            model
 
 let viewScene (model : MModel) =
     Sg.box (Mod.constant C4b.Green) (Mod.constant Box3d.Unit)
@@ -29,31 +33,32 @@ let viewScene (model : MModel) =
             do! DefaultSurfaces.simpleLighting
         }
 
-
 let view (model : MModel) =
 
     let renderControl =
        FreeFlyController.controlledControl model.cameraState Camera (Frustum.perspective 60.0 0.1 100.0 1.0 |> Mod.constant) 
-                    (AttributeMap.ofList [ 
-                        style "width: 100%; grid-row: 2; height:100%"; 
-                        attribute "showFPS" "true";         // optional, default is false
-                        //attribute "showLoader" "false"    // optional, default is true
-                        //attribute "data-renderalways" "1" // optional, default is incremental rendering
-                        attribute "data-samples" "8"        // optional, default is 1
-                    ]) 
+                    (AttributeMap.ofList [ style "width: 400px; height: 400px; background: #222"; attribute "data-samples" "8"]) 
                     (viewScene model)
 
+    let channel = model.cameraState.view
+                    |> Mod.map (fun v -> v.Forward)
+                    |> Mod.channel
 
-    div [style "display: grid; grid-template-rows: 40px 1fr; width: 100%; height: 100%" ] [
-        div [style "grid-row: 1"] [
+    let updateData = "foo.onmessage = function (data) { console.log('got camera view update: ' + data); }"
+
+    onBoot' ["foo", channel] updateData (
+        div [] [
             text "Hello 3D"
             br []
             button [onClick (fun _ -> CenterScene)] [text "Center Scene"]
+            br []
+            button (Html.IO.fileDialog (fun s -> SetFiles [s])) [text "abc"]
+            br []
+            renderControl
         ]
-        renderControl
-        br []
-        text "use first person shooter WASD + mouse controls to control the 3d scene"
-    ]
+    )
+
+
 
 let threads (model : Model) = 
     FreeFlyController.threads model.cameraState |> ThreadPool.map Camera
