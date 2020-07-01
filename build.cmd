49e4db640429afa2cc759a006f38a49c3f10b322
Thomas Ortner <ortner@vrvis.at>
2020-07-01 15:04:53 +0200

    updated build script and lock file

--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@ SETLOCAL
 PUSHD %~dp0
 
 IF NOT exist .paket\paket.exe (
-	dotnet tool install Paket --tool-path .paket --version 6.0.0-alpha007
+	dotnet tool install Paket --tool-path .paket --version [5.242.2]
 )
 
 if NOT exist paket.lock (
