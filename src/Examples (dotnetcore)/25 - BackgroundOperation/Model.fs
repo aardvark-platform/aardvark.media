786204aea6f06e3405c637f3a1effc1fb6db3ba6
Harald Steinlechner <haraldsteinlechner@gmail.com>
2020-11-11 17:26:38 +0100

    added Example which demonstrates asynchronous background operations

---
+++ src/Examples (dotnetcore)/25 - BackgroundOperation/Model.fs
@@ -0,0 +1,27 @@
+﻿namespace RenderControl.Model
+
+open Aardvark.Base
+open FSharp.Data.Adaptive
+open Aardvark.UI.Primitives
+open Adaptify
+
+type Message = 
+    | Camera of FreeFlyController.Message
+    | CenterScene
+    | ToggleBackground
+    | UpdateInput of V2i
+    | Result of string
+    | Progress of string
+
+
+
+
+[<ModelType>]
+type Model = 
+    {
+        cameraState : CameraControllerState
+        background : C4b
+        currentInput : MVar<V2i>
+        result : string
+        progress : string
+    }
