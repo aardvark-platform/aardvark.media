0a844fb3f68b2112bd2876e3b8fe16a43ec25284
Harald Steinlechner <haraldsteinlechner@gmail.com>
2021-03-31 11:07:15 +0200

    added golden layout example

---
+++ src/Scratch/25 - GoldenLayout/App.fs
@@ -0,0 +1,54 @@
+﻿module Inc.App
+open Aardvark.UI
+open Aardvark.UI.Primitives
+
+open Aardvark.Base
+open FSharp.Data.Adaptive
+open Aardvark.Rendering
+open Inc.Model
+
+let update (model : Model) (msg : Message) =
+    match msg with
+        Inc -> { model with value = model.value + 1 }
+
+let dependencies = [
+    { name = "Golden"; url = "http://golden-layout.com/files/latest/js/goldenlayout.js"; kind = Script }
+    { name = "GoldenCSS"; url = "http://golden-layout.com/files/latest/css/goldenlayout-base.css"; kind = Stylesheet }
+    { name = "GoldenAard"; url = "./goldenAard.js"; kind = Stylesheet }
+]
+
+let view (model : AdaptiveModel) =
+    require dependencies (
+        onBoot "initLayout()" (
+            div [] [
+                text "Hello World"
+                br []
+                button [onClick (fun _ -> Inc)] [text "Increment"]
+                text "    "
+                Incremental.text (model.value |> AVal.map string)
+                br []
+                img [
+                    attribute "src" "https://upload.wikimedia.org/wikipedia/commons/6/67/SanWild17.jpg"; 
+                    attribute "alt" "aardvark"
+                    style "max-width: 80%; max-height: 80%"
+                ]
+            ]
+        )
+    )
+
+
+let threads (model : Model) = 
+    ThreadPool.empty
+
+
+let app =                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+    {
+        unpersist = Unpersist.instance     
+        threads = threads 
+        initial = 
+            {   
+                value = 0
+            }
+        update = update 
+        view = view
+    }
---
+++ src/Scratch/25 - GoldenLayout/resources/goldenAard.js
@@ -0,0 +1,31 @@
+﻿function initLayout() {
+    var layout = new GoldenLayout({
+        settings:{
+            hasHeaders: true,
+            constrainDragToContainer: true,
+            reorderEnabled: true,
+            selectionEnabled: false,
+            popoutWholeStack: false,
+            blockedPopoutsThrowError: true,
+            closePopoutsOnUnload: true,
+            showPopoutIcon: true,
+            showMaximiseIcon: true,
+            showCloseIcon: true
+        },
+        dimensions: {
+            borderWidth: 5,
+            minItemHeight: 10,
+            minItemWidth: 10,
+            headerHeight: 20,
+            dragProxyWidth: 300,
+            dragProxyHeight: 200
+        },
+        labels: {
+            close: 'close',
+            maximise: 'maximise',
+            minimise: 'minimise',
+            popout: 'open in new window'
+        },
+        content: [ /* see item config */ ]
+    });
+};
