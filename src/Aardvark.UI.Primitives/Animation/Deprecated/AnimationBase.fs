596e470e969adb54dbae6803a6fcd02b07e9feb9
ThomasOrtner <ortner.t@googlemail.com>
2021-08-27 11:56:22 +0200

    added animateForwardAndLocation to deprecated animation code (used in Dibit and PRo3D) with corrected final interation. All other deprecated animations do not reach their target.

--- src/Aardvark.UI.Primitives/Animation/Deprecated/AnimationBase.fs
+++ src/Aardvark.UI.Primitives/Animation/Deprecated/AnimationBase.fs
@@ -1,63 +1,63 @@
 namespace Aardvark.UI.Animation
 
 module AnimationApp = 
-  open Aardvark.Base
-  open FSharp.Data.Adaptive
-  open FSharp.Data.Adaptive.Operators
-  open Aardvark.Rendering
-  
-  open Aardvark.UI
-              
-  let shouldAnimate (m : AnimationModel) =
-        m.animation = Animate.On && IndexList.count m.animations > 0
+    open Aardvark.Base
+    open FSharp.Data.Adaptive
+    open FSharp.Data.Adaptive.Operators
+    open Aardvark.Rendering
     
-  let updateAnimation (m : 'm) (t : Aardvark.UI.Animation.Time) (a : Animation<'m,'s,'a>) =
-        match a.state with
-            | None ->
-                let s = a.start m
-                { a with state = Some s; startTime = Some t}, 0.0, s
-            | Some s -> a, t-a.startTime.Value, s
-
-  let update (m : AnimationModel) (msg : AnimationAction ) =
-    match msg with
-    | Tick t when shouldAnimate m -> 
-        match IndexList.tryAt 0 m.animations with
+    open Aardvark.UI
+                
+    let shouldAnimate (m : AnimationModel) =
+          m.animation = Animate.On && IndexList.count m.animations > 0
+      
+    let updateAnimation (m : 'm) (t : Aardvark.UI.Animation.Time) (a : Animation<'m,'s,'a>) =
+          match a.state with
+              | None ->
+                  let s = a.start m
+                  { a with state = Some s; startTime = Some t}, 0.0, s
+              | Some s -> a, t-a.startTime.Value, s
+    
+    let update (m : AnimationModel) (msg : AnimationAction ) =
+        match msg with
+        | Tick t when shouldAnimate m -> 
+            match IndexList.tryAt 0 m.animations with
             | Some anim -> 
                 // initialize animation (if needed)
                 let (anim,localTime,state) = updateAnimation m t anim
                 match anim.sample(localTime, t) state with 
-                    | None -> { m with animations = IndexList.removeAt 0 m.animations } // animation stops, so pop it
-                    | Some (s,cameraView) -> 
-                        // feed in new state
-                        let anim = { anim with state = Some s } 
-                        // activate result in camera state
-                        //{ m with cameraState = { m.cameraState with view = cameraView }; 
-                        { m with cam = cameraView; animations = IndexList.setAt 0 anim m.animations }
+                | None -> { m with animations = IndexList.removeAt 0 m.animations } // animation stops, so pop it
+                | Some (s,cameraView) -> 
+                    // feed in new state
+                    let anim = { anim with state = Some s } 
+                    // activate result in camera state
+                    //{ m with cameraState = { m.cameraState with view = cameraView }; 
+                    { m with cam = cameraView; animations = IndexList.setAt 0 anim m.animations }
             | None -> m
-    | PushAnimation a -> 
-        { m with animations = IndexList.add a m.animations }
-    | RemoveAnimation i -> 
-        { m with animations = IndexList.remove i m.animations }
-    | Tick _ -> m // not allowed to animate      
-
-  let totalTime = System.Diagnostics.Stopwatch.StartNew()
-
-  let rec time() =
-      proclist {
-          do! Proc.Sleep 10
-          yield Tick totalTime.Elapsed.TotalSeconds
-          yield! time()
-      }
+        | PushAnimation a -> 
+            { m with animations = IndexList.add a m.animations }
+        | RemoveAnimation i -> 
+            { m with animations = IndexList.remove i m.animations }
+        | Tick _ -> m // not allowed to animate      
     
-  module ThreadPool =
-      let unionMany xs = List.fold ThreadPool.union ThreadPool.empty xs
+    let totalTime = System.Diagnostics.Stopwatch.StartNew()
     
-      let threads (m : AnimationModel) =                                        
-        // handling of continous animations
-        
-        if shouldAnimate m then
-          ThreadPool.add "timer" (time()) ThreadPool.empty
-        else
-          ThreadPool.empty
+    let rec time() =
+        proclist {
+            do! Proc.Sleep 10
+            yield Tick totalTime.Elapsed.TotalSeconds
+            yield! time()
+        }
+      
+    module ThreadPool =
+        let unionMany xs = List.fold ThreadPool.union ThreadPool.empty xs
+      
+        let threads (m : AnimationModel) =                                        
+          // handling of continous animations
+          
+          if shouldAnimate m then
+            ThreadPool.add "timer" (time()) ThreadPool.empty
+          else
+            ThreadPool.empty
         
         
--- src/Aardvark.UI.Primitives/Animation/Deprecated/Model.fs
+++ src/Aardvark.UI.Primitives/Animation/Deprecated/Model.fs
@@ -36,104 +36,152 @@ type AnimationAction =
  | RemoveAnimation of Index   
         
 module CameraAnimations =
-  open Aardvark.Base.Trafo
+    open Aardvark.Base.Trafo
+    
+    let initial name = 
+        {
+            startTime = None
+            state = None
+            name = name
+            start = fun m -> m.cam // initially, grab camera controller state
+            sample = fun (_,_) (state : CameraView) -> None
+        }
+    
+    let zoom (dir : float) (name : string) = 
+        {
+            (initial name) with 
+                sample = fun (localTime, globalTime) (state : CameraView) -> // given the state and t since start of the animation, compute a state and the cameraview
+                    if localTime < 1.0 then
+                        let view = state.WithLocation(state.Location + dir * state.Forward * (localTime / 1.0))
+                        Some (state,view)
+                    else None
+        }
+    
+    let animateLocation (destination : V3d) (duration : RelativeTime) (name : string) = 
+        {
+            (initial name) with 
+                sample = fun (localTime, globalTime) (state : CameraView) -> // given the state and t since start of the animation, compute a state and the cameraview
+                    if localTime < duration then
+                        let vec      = destination - state.Location
+                        let velocity = vec.Length / duration
+                        let dir      = vec.Normalized
+                        
+                        let location' = state.Location + dir * velocity * localTime
+                        let view = state.WithLocation(location')
+                        
+                        Some (state,view)
+                    else 
+                        None
+        }
+    
+    let animateLocationFixedLookAt (destination : V3d) (lookAt : V3d) (duration : RelativeTime) (name : string) = 
+        {
+            (initial name) with 
+                sample = fun (localTime, globalTime) (state : CameraView) -> // given the state and t since start of the animation, compute a state and the cameraview
+                    if localTime < duration then
+                        let vec      = destination - state.Location
+                        let velocity = vec.Length / duration                  
+                        let dir      = vec.Normalized                  
+        
+                        let location' = state.Location + dir * velocity * localTime
+                        let forward = (lookAt - location').Normalized
+        
+                        let view = 
+                            state 
+                            |> CameraView.withLocation(location') 
+                            |> CameraView.withForward forward
+        
+                        Some (state,view)
+                    else None
+        }
+    
+    let animateLookAt (src : V3d)(dst : V3d) (duration : RelativeTime) (name : string) = 
+        {
+            (initial name) with 
+                sample = fun (localTime, globalTime) (state : CameraView) -> // given the state and t since start of the animation, compute a state and the cameraview
+                    if localTime < duration then
+                        let vec      = dst - src
+                        let velocity = vec.Length / duration
+                        let dir      = vec.Normalized
+        
+                        let lookAt' = src + dir * velocity * localTime
+                        let forward = (lookAt' - state.Location).Normalized
+        
+                        let view = state |> CameraView.withForward forward                  
+                                        
+                        Some (state,view)
+                    else 
+                        None
+        }
+    
+    let animateFoward (dst : V3d) (duration : RelativeTime) (name : string) = 
+        {
+            (initial name) with 
+                sample = fun (localTime, globalTime) (state : CameraView) -> // given the state and t since start of the animation, compute a state and the cameraview
+                    if localTime < duration then                  
+                        let rot = Rot3d.RotateInto(state.Forward, dst) * localTime / duration
+                        let forward' = Rot3d(rot).Transform state.Forward               
+                        let view = state |> CameraView.withForward forward'
+        
+                        Some (state,view)
+                    else 
+                        None
+        }
+    
+    let animateSky (dst : V3d) (duration : RelativeTime) (name : string) = 
+        {
+            (initial name) with 
+                sample = fun (localTime, globalTime) (state : CameraView) -> // given the state and t since start of the animation, compute a state and the cameraview
+                    if localTime < duration then
+                        let rot = Rot3d.RotateInto(state.Up, dst) * localTime / duration
+                        let sky' = Rot3d(rot).Transform(state.Up)
+                        let view = state |> CameraView.withUp sky'
+        
+                        Some (state,view)
+                    else 
+                        None
+        }
 
-  let initial name = 
-    {
-      startTime = None
-      state = None
-      name = name
-      start = fun m -> m.cam // initially, grab camera controller state
-      sample = fun (_,_) (state : CameraView) -> None
-    }
+    let private transformLocationForwardUp (pos: V3d) (dir: V3d) (up:V3d) (duration : RelativeTime) (localTime : RelativeTime) (state : CameraView) =
+        let rot      = Rot3d.RotateInto(state.Forward, dir) * localTime / duration |> Rot3d |> Trafo3d
+        let forward  = rot.Forward.TransformDir state.Forward
 
-  let zoom (dir : float) (name : string) = 
-    {
-      (initial name) with 
-        sample = fun (localTime, globalTime) (state : CameraView) -> // given the state and t since start of the animation, compute a state and the cameraview
-          if localTime < 1.0 then
-              let view = state.WithLocation(state.Location + dir * state.Forward * (localTime / 1.0))
-              Some (state,view)
-          else None
-    }
+        let uprot     = Rot3d.RotateInto(state.Up, up) * localTime / duration |> Rot3d |> Trafo3d
+        let up        = uprot.Forward.TransformDir state.Up
+                      
+        let vec       = pos - state.Location
+        let velocity  = vec.Length / duration                  
+        let dir       = vec.Normalized
+        let location  = state.Location + dir * velocity * localTime
+         
+        state 
+        |> CameraView.withForward forward
+        |> CameraView.withUp up
+        |> CameraView.withLocation location
 
-  let animateLocation (destination : V3d) (duration : RelativeTime) (name : string) = 
-    {
-      (initial name) with 
-        sample = fun (localTime, globalTime) (state : CameraView) -> // given the state and t since start of the animation, compute a state and the cameraview
-          if localTime < duration then
-            let vec      = destination - state.Location
-            let velocity = vec.Length / duration                  
-            let dir      = vec.Normalized
+    let animateForwardAndLocation (pos: V3d) (dir: V3d) (up:V3d) (duration: RelativeTime) (name: string) = 
+        {
+            (initial name) with
+                sample = fun (localTime, globalTime) (state : CameraView) -> // given the state and t since start of the animation, compute a state and the cameraview                    
+                    let cameraChange = not (state.Location.ApproximateEquals(pos) && state.Forward.ApproximateEquals(dir) && state.Up.ApproximateEquals(up))                    
 
-            let location' = state.Location + dir * velocity * localTime
-            let view = state.WithLocation(location')
+                    if cameraChange then
+                        if localTime <= duration then
+                            let view = transformLocationForwardUp pos dir up duration localTime state
+                            Some (state, view)
+                        else                                                
+                            if state.Location.IsNaN |> not then
+                                //on the last iteration set current position to endpoint - localtime = duration
+                                let view = 
+                                    transformLocationForwardUp pos dir up duration duration state
 
-            Some (state,view)
-          else None
-    }
+                                let state =
+                                    state
+                                    |> CameraView.withLocation V3d.NaN
 
-  let animateLocationFixedLookAt (destination : V3d) (lookAt : V3d) (duration : RelativeTime) (name : string) = 
-    {
-      (initial name) with 
-        sample = fun (localTime, globalTime) (state : CameraView) -> // given the state and t since start of the animation, compute a state and the cameraview
-          if localTime < duration then
-            let vec      = destination - state.Location
-            let velocity = vec.Length / duration                  
-            let dir      = vec.Normalized                  
-
-            let location' = state.Location + dir * velocity * localTime
-            let forward = (lookAt - location').Normalized
-
-            let view = 
-              state 
-                |> CameraView.withLocation(location') 
-                |> CameraView.withForward forward
-
-            Some (state,view)
-          else None
-    }
-
-  let animateLookAt (src : V3d)(dst : V3d) (duration : RelativeTime) (name : string) = 
-    {
-      (initial name) with 
-        sample = fun (localTime, globalTime) (state : CameraView) -> // given the state and t since start of the animation, compute a state and the cameraview
-          if localTime < duration then
-            let vec      = dst - src
-            let velocity = vec.Length / duration
-            let dir      = vec.Normalized
-
-            let lookAt' = src + dir * velocity * localTime
-            let forward = (lookAt' - state.Location).Normalized
-
-            let view = state |> CameraView.withForward forward                  
-                            
-            Some (state,view)
-          else None
-    }
-
-  let animateFoward (dst : V3d) (duration : RelativeTime) (name : string) = 
-    {
-      (initial name) with 
-        sample = fun (localTime, globalTime) (state : CameraView) -> // given the state and t since start of the animation, compute a state and the cameraview
-          if localTime < duration then                  
-            let rot = Rot3d.RotateInto(state.Forward, dst) * localTime / duration
-            let forward' = Rot3d(rot).Transform state.Forward               
-            let view = state |> CameraView.withForward forward'
-
-            Some (state,view)
-          else None
-    }
-
-  let animateSky (dst : V3d) (duration : RelativeTime) (name : string) = 
-    {
-      (initial name) with 
-        sample = fun (localTime, globalTime) (state : CameraView) -> // given the state and t since start of the animation, compute a state and the cameraview
-          if localTime < duration then                  
-            let rot = Rot3d.RotateInto(state.Up, dst) * localTime / duration
-            let sky' = Rot3d(rot).Transform(state.Up)                  
-            let view = state |> CameraView.withUp sky'
-
-            Some (state,view)
-          else None
-    }
+                                Some (state , view)
+                            else
+                                None
+                    else
+                        None
+        }
