46610e4b964f49d9c681d17995bd9fca386ae06a
Martin <mautner@vrvis.at>
2024-08-06 18:58:05 +0200

    [Primitives] Remove List.intersperse

--- src/Aardvark.UI.Primitives/Primitives/UI.Primitives.fs
+++ src/Aardvark.UI.Primitives/Primitives/UI.Primitives.fs
@@ -11,14 +11,6 @@ open Aardvark.UI.Primitives
 
 type NumericInputType = Slider | InputBox
 
-module List =
-    /// The intersperse function takes an element and a list and
-    /// 'intersperses' that element between the elements of the list.
-    let intersperse sep ls =
-        List.foldBack (fun x -> function
-            | [] -> [x]
-            | xs -> x::sep::xs) ls []
-
 module Html =
 
     module Layout =
