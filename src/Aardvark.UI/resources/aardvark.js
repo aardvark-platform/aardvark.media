304b198de781b820df85e5069c97638a55b010e8
Christian Luksch <luksch@vrvis.at>
2026-02-18 16:11:48 +0100

    fixed script/stylesheet loading not delaying further promises created in eventSocket.onmessage: originally onmessage called addReferences within a promise that only then again chained a promise that loaded the scripts -> other onmessages that arrive before the addReferences promise was executed chained their promises before

--- src/Aardvark.UI/resources/aardvark.js
+++ src/Aardvark.UI/resources/aardvark.js
@@ -870,73 +870,66 @@ class Renderer {
 }
 
 if (!aardvark.addReferences) {
-    aardvark.addReferences = function (refs, realCont) {
-        
-        aardvark.promise = aardvark.promise.then(function () {
-            return new Promise(function (s, e) {
-                var cont = function () { realCont(); s(); };
+    aardvark.addReferences = function (refs, userCode) {
 
-                function acc(i) {
-                    if (i >= refs.length) {
-                        return cont;
-                    }
-                    else {
-                        var ref = refs[i];
-                        var kind = ref.kind;
-                        var name = ref.name;
-                        var url = ref.url;
-                        if (kind === "script" || kind === "module") {
-                            if (!aardvark.referencedScripts[name]) {
-                                aardvark.referencedScripts[name] = true;
-                                return function () {
-                                    var script = document.createElement("script");
-                                    var cc = function (evt) {
-                                        console.debug(`[Aardvark] referenced ${kind} "${name}" (${url})`);
-                                        acc(i + 1)();
-                                    };
-                                    var err = function (evt) {
-                                        console.warn(`[Aardvark] failed to referenced ${kind} "${name}" (${url})`);
-                                        acc(i + 1)();
-                                    };
-                                    script.src = url;
-                                    script.async = true;
-                                    if (kind === "module") script.type = "module";
-                                    script.addEventListener("load", cc);
-                                    script.addEventListener("error", err);
-                                    document.getElementsByTagName("script")[0].parentNode.appendChild(script);
-                                };
-                            }
-                            else return acc(i + 1);
-                        }
-                        else {
-                            if (!aardvark.referencedStyles[name]) {
-                                aardvark.referencedStyles[name] = true;
-                                return function () {
-                                    var script = document.createElement("link");
-                                    var cc = function (evt) {
-                                        console.debug("[Aardvark] referenced stylesheet \"" + name + "\" (" + url + ")");
-                                        acc(i + 1)();
-                                    };
-                                    var err = function (evt) {
-                                        console.warn("[Aardvark] failed to reference stylesheet \"" + name + "\" (" + url + ")");
-                                        acc(i + 1)();
-                                    };
-                                    script.addEventListener("load", cc);
-                                    script.addEventListener("error", err);
-                                    script.setAttribute("rel", "stylesheet");
-                                    script.setAttribute("href", url);
-                                    document.head.appendChild(script);
-                                };
-                            }
-                            else return acc(i + 1);
-                        }
+        function loadScript(ref) {
+            return new Promise((resolve, reject) => {
 
-                    }
+                const kind = ref.kind;
+                const name = ref.name;
+                const url = ref.url;
+
+                const key = `${name}-${kind}`; // allow using identical names for stylesheet and scripts
+
+                if (aardvark.referencedScripts[key]) {
+                    return resolve();
                 }
-                var real = acc(0);
-                real();
+
+                aardvark.referencedScripts[key] = true;
+
+                if (kind === "script" || kind === "module") {
+                
+                    const script = document.createElement("script");
+                    const cc = function (evt) {
+                        console.debug(`[Aardvark] referenced ${kind} "${name}" (${url})`);
+                        resolve();
+                    };
+                    const err = function (evt) {
+                        console.warn(`[Aardvark] failed to referenced ${kind} "${name}" (${url})`);
+                        resolve();
+                    };
+                    script.src = url;
+                    script.async = true;
+                    if (kind === "module") script.type = "module";
+                    script.addEventListener("load", cc);
+                    script.addEventListener("error", err);
+                    document.getElementsByTagName("script")[0].parentNode.appendChild(script);
+                }
+                else {   
+                    
+                    const script = document.createElement("link");
+                    const cc = function (evt) {
+                        console.debug("[Aardvark] referenced stylesheet \"" + name + "\" (" + url + ")");
+                        resolve();
+                    };
+                    const err = function (evt) {
+                        console.warn("[Aardvark] failed to reference stylesheet \"" + name + "\" (" + url + ")");
+                        resolve();
+                    };
+                    script.addEventListener("load", cc);
+                    script.addEventListener("error", err);
+                    script.setAttribute("rel", "stylesheet");
+                    script.setAttribute("href", url);
+                    document.head.appendChild(script);
+                }
             });
+        }
+
+        refs.forEach(ref => {
+            aardvark.promise = aardvark.promise.then(() => loadScript(ref));
         });
+
+        aardvark.promise = aardvark.promise.then(() => userCode());
     };
 }
 
@@ -1068,21 +1061,26 @@ if (!aardvark.connect) {
             doPing();
         };
 
-        var exectutedCode = "";
-
         eventSocket.onmessage = function (m) {
+
             var c = m.data.substring(0, 1);
             if (c === "x") {
-                var data = m.data.substring(1, m.data.length);
-                aardvark.promise = aardvark.promise.then(function () {
-                    try {
-                        //exectutedCode = exectutedCode + "\r\n\r\n\r\n" + data;
-                        (new Function("{\r\n" + data + "\r\n}"))();
-                    } catch (e) {
-                        console.warn("could not execute event message with exn " + e + ":\n" + data);
-                        debugger;
-                    }
-                });
+                const code = m.data.substring(1, m.data.length);
+                // check if user code includes loading of scripts or stylesheets
+                if (code.startsWith("aardvark.addReferences")) { 
+                    // addReferences function directly chains script/stylesheet loading and user code execution in aardvark.promise chain
+                    (new Function("{\r\n" + code + "\r\n}"))();
+                }
+                else {
+                    aardvark.promise = aardvark.promise.then(function () {
+                        try {
+                            (new Function("{\r\n" + code + "\r\n}"))();
+                        } catch (e) {
+                            console.warn("could not execute event message with exn " + e + ":\n" + code);
+                            debugger;
+                        }
+                    });
+                }
             }
             else {
                 var data = m.data;
